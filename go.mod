529df9f894388246aeb675ddda9115396b1de88d
ginonpan <hityorugf@gmail.com>
2019-12-06 19:00:58 +0900

    keybord input

---
+++ go.mod
@@ -0,0 +1,6 @@
+module musigo
+
+require (
+	github.com/algoGuy/EasyMIDI v0.0.0-20180322051653-708ca39e7399
+	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	input "musigo/actions"
+	"os"
+	"time"
+
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+const (
+	version = "1.0.0"
+	appName = "musigo"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = appName
+	app.Version = version
+	app.Compiled = time.Now()
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "input-score",
+			Usage:  "標準入力を受け付ける",
+			Action: input.Input,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "output, o",
+					Usage: "出力するファイル名",
+				},
+			},
+		},
+		{
+			Name:   "sample-read",
+			Usage:  "midiを読み込む(sample)",
+			Action: read.Read,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "file, f",
+					Usage: "入力するファイル名",
+				},
+			},
+		},
+		{
+			Name:   "sample-create",
+			Usage:  "midiを書き出す(sample)",
+			Action: create.Create,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "output, o",
+					Usage: "出力するファイル名",
+				},
+			},
+		},
+	}
+	app.Run(os.Args)
+}
